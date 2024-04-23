a6c265ee4ecdf22162cf0fec06cdd7e77a9db191
UmmeKulsumTumpa <bsse1307@iit.du.ac.bd>
2024-04-24 00:49:08 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Newsletter from './components/Newsletter';
 import Publications from './components/Publications';
+import Team from './components/Meet_with_the_team';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Publications />
+      <Team />
       <Cards />
       <Footer />
     </div>
